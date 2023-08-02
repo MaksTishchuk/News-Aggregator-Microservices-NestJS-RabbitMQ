254537087aa5c54826db2c36264775d3c1b355ac
Maksym Tishchuk <58475629+MaksTishchuk@users.noreply.github.com>
2023-08-02 17:34:21 +0300

    add validations

--- api-gateway/src/user/dto/update-user-profile.dto.ts
+++ api-gateway/src/user/dto/update-user-profile.dto.ts
@@ -1,20 +1,25 @@
-import {IsEnum, IsOptional} from 'class-validator';
+import {IsEnum, IsOptional, IsString} from 'class-validator';
 import {GenderEnum} from "./enums/gender.enum";
 
 export class UpdateUserProfileDto {
   @IsOptional()
+  @IsString()
   firstName?: string;
 
   @IsOptional()
+  @IsString()
   lastName?: string;
 
   @IsOptional()
+  @IsString()
   phoneNumber?: string;
 
   @IsOptional()
+  @IsString()
   country?: string;
 
   @IsOptional()
+  @IsString()
   city?: string;
 
   @IsOptional()
